faed959297e2fa4657a6a06310c4c5c4eeb4f553
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-29 17:49:34 -0700

    Replaced icons, added a basic navbar, main div, and footer.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import NavbarComponent from './components/NavbarComponent';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<NavbarComponent />
+			</header>
+			<div className="main-container">
+				<Main />
+			</div>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
 }
 
 export default App;
